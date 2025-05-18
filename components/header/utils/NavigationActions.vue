edd739292a10ee728fb3e04963a76194832eeca5
aboubakar DIAKITE <aboubakarsiriki60@gmail.com>
2025-05-18 22:54:13 +0200

    feat: lang localStorage

--- components/header/utils/NavigationActions.vue
+++ components/header/utils/NavigationActions.vue
@@ -1,17 +1,30 @@
 <script setup lang="ts">
-import {ref} from 'vue'
+import {ref, onMounted} from 'vue'
 import LanguageComponent from "~/components/header/utils/components/LanguageComponent.vue";
 import LocationContextComponent from "~/components/header/utils/components/LocationContextComponent.vue";
 
-const {setLocale} = useI18n()
+const {setLocale, locale} = useI18n()
 
 const showLanguageMenu = ref(false)
 const flag = ref('🇫🇷')
+
+onMounted(() => {
+  const savedLocale = localStorage.getItem('locale')
+  const savedFlag = localStorage.getItem('flag')
+
+  if (savedLocale) {
+    setLocale(savedLocale as 'fr' | 'en' | 'es')
+    flag.value = savedFlag || '🇫🇷'
+  }
+})
 
 async function changeLanguage(lo: 'fr' | 'en' | 'es', flagEmoji: string) {
   await setLocale(lo)
   showLanguageMenu.value = false
   flag.value = flagEmoji
+
+  localStorage.setItem('locale', lo)
+  localStorage.setItem('flag', flagEmoji)
 }
 
 function toggleLanguageMenu(value: boolean) {
@@ -25,4 +38,3 @@ function toggleLanguageMenu(value: boolean) {
     <LanguageComponent @change-language="changeLanguage" :show-language-menu="showLanguageMenu" />
   </div>
 </template>
-
