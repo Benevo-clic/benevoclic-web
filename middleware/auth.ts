04d1f60481ead0f4f6d907c54f884a5dc5af43f0
aboubakar DIAKITE <aboubakarsiriki60@gmail.com>
2025-06-19 23:55:27 +0200

    feat: register association

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,40 +1,13 @@
 import {defineNuxtRouteMiddleware} from "#app";
 import {useUserStore} from "~/stores/user/user.store";
-import {onMounted} from "vue";
-import {useUser} from "~/composables/auth/useUser";
+
 
 export default defineNuxtRouteMiddleware((to, from) => {
    const  authStore= useUserStore()
 
-    const auth = useUser()
 
-    onMounted(async () => {
-
-        await auth.fetchUser()
-
-        const userRole = auth.userRole
-
-
-        if (authStore.isAuthenticated) {
-            return navigateTo('/auth/login')
-        }
-
-        if(userRole.value === 'VOLUNTEER') {
-            return navigateTo(
-                {
-                    path: '/auth/registerVolunteer',
-                }
-            )
-        }
-
-        if(userRole.value === 'ASSOCIATION'){
-            return navigateTo(
-                {
-                    path: '/auth/registerAssociation',
-                }
-            )
-        }
-    })
-
+    if (authStore.isAuthenticated) {
+        return navigateTo('/auth/login')
+    }
 
 })
