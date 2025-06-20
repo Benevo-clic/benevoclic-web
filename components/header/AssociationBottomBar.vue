d433976aff2082fce18718b1e4579ae1619ff16d
aboubakar DIAKITE <aboubakarsiriki60@gmail.com>
2025-06-20 14:24:11 +0200

    feat: header association

--- components/header/AssociationBottomBar.vue
+++ components/header/AssociationBottomBar.vue
@@ -23,13 +23,13 @@ function handleManageEvents() {
   <div class="flex items-center justify-center">
     <div class="flex justify-center flex-wrap text-base-content gap-4">
       <button class="btn btn-primary btn-sm px-3 py-1 flex items-center gap-1" @click="handleAddNewEvent">
-        <PlusIcon class="w-5 h-5" /> Nouvel événement
+        <PlusIcon class="w-5 h-5" /> {{$t('association.activity.new_event')}}
       </button>
       <button class="btn btn-ghost btn-sm px-2 py-0 flex items-center gap-1" @click="handlePastEvents">
-        <HistoryIcon class="w-6 h-6" /> Événements passés
+        <HistoryIcon class="w-6 h-6" /> {{$t('association.activity.history_events')}}
       </button>
       <button class="btn btn-ghost btn-sm px-2 py-0 flex items-center gap-1" @click="handleManageEvents">
-        <CalendarIcon class="w-6 h-6" /> Gérer les événements
+        <CalendarIcon class="w-6 h-6" /> {{$t('association.activity.manage_events')}}
       </button>
       <button class="btn btn-ghost btn-sm px-2 py-0 flex items-center gap-1" @click="navigateTo('/help')">
         <HelpIcon class="w-6 h-6" /> Aide
