b0f9897fdc829f21062f3e1287e6f21174294d45
aboubakar DIAKITE <aboubakarsiriki60@gmail.com>
2025-08-10 15:39:34 +0200

    fix: migrate environment variables from useRuntimeConfig to process.env for server-side and fix Firebase client-side config

--- plugins/firebase-permissions.client.ts
+++ plugins/firebase-permissions.client.ts
@@ -6,14 +6,8 @@ export default defineNuxtPlugin(() => {
         const { getAuth, GoogleAuthProvider, browserPopupRedirectResolver } = await import(
           'firebase/auth'
         )
-        const firebaseConfig = {
-          apiKey: process.env.API_KEY,
-          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-          projectId: process.env.FIREBASE_PROJECT_ID,
-          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-          appId: process.env.FIREBASE_APP_ID
-        }
+        const config = useRuntimeConfig()
+        const firebaseConfig = config.public.firebaseConfig
         if (!firebaseConfig.apiKey || !firebaseConfig.authDomain) {
           console.log('Configuration Firebase manquante ou invalide', firebaseConfig)
           throw new Error('Configuration Firebase manquante ou invalide')
